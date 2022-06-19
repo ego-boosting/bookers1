28059f451cf2d3c005b27e3c25cbd90b1d64c535
EC2 Default User <kazukipump7@gmail.com>
2022-06-19 07:17:43 +0000

    add flash message bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -41,6 +41,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     redirect_to books_path  # 投稿一覧画面へリダイレクト
+    flash[:notice] = "Book was successfully destroyed."
   end
 
   private
